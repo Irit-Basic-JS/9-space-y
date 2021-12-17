5b1195d2454499001a7f867fad1481b1eab08c85
xirce <dgorohov78@gmail.com>
2021-12-17 22:55:55 +0500

    1-8 tasks done.

--- server.mjs
+++ server.mjs
@@ -3,25 +3,60 @@ import fs from "fs";
 import express from "express";
 import https from "https";
 import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
-import fetch from "node-fetch";
 
 const rootDir = process.cwd();
 const port = 3000;
 const app = express();
 
+const validUrls = ['login', 'api', 'static'];
+const redirectingMiddleware = function (req, res, next) {
+    const root = req.url.split('/')[1];
+    const shouldBeSkipped = validUrls.includes(root);
+    const isFile = path.extname(root) !== '';
+    if (shouldBeSkipped || isFile || req.cookies.username) {
+        return next()
+    } else {
+        res.redirect('/login');
+    }
+};
+
+app.use(express.static('spa/build'));
+app.use(cookieParser());
+app.use(redirectingMiddleware);
+
 app.get("/client.mjs", (_, res) => {
-  res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
-  res.sendFile(path.join(rootDir, "client.mjs"), {
-    maxAge: -1,
-    cacheControl: false,
-  });
+    res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+    res.sendFile(path.join(rootDir, "client.mjs"), {
+        maxAge: -1,
+        cacheControl: false,
+    });
 });
 
-app.get("/", (_, res) => {
-  res.send(":)");
+app.get('/api/login', (req, res) => {
+    const username = req.query.username;
+    res.cookie('username', username, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'Strict'
+    });
+    res.json({ username });
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.get('/api/user', (req, res) => {
+    const username = req.cookies.username;
+    res.json({ username });
+})
+
+app.get('/api/logout', (req, res) => {
+    res.clearCookie('username');
+    res.redirect('/');
 });
+
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(rootDir, 'spa', 'build', 'index.html'));
+})
+
+https.createServer({
+    key: fs.readFileSync("certs/server.key"),
+    cert: fs.readFileSync("certs/server.cert"),
+}, app).listen(port, () => console.log(`App listening on port ${port}`));
